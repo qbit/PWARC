bcc60cc30478c3ce13a8bf5074eee2967f26828d
Aaron Bieber <deftly@gmail.com>
2014-02-09 20:47:15 -0700

    thumbs

--- images/thumb.sh
+++ images/thumb.sh
@@ -1,5 +1,9 @@
 #!/bin/ksh
 
-for i in $(ls *.jpg); do
+for i in $(ls *.jpg | grep -v ^m); do
+	convert -thumbnail 150x $i m${i}
+done
+
+for i in $(ls *.JPG | grep -v ^m); do
 	convert -thumbnail 150x $i m${i}
 done
